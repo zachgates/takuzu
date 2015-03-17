f09f55f08392891db40d87de8194750cd213104f
Zach <zachgatestech@gmail.com>
2015-03-17 15:18:12 -0500

    new Game object

---
+++ js/game.js
@@ -0,0 +1,24 @@
+function Game(grid) {
+	var self = this;
+	
+	self.gametable = grid
+	
+	function solve() {
+		// fade in solution
+	}
+	
+	function exit() {
+		// fade back to menu
+	}
+	
+	this.endGame = function() {
+		// fade out
+		// exit
+	}
+	
+	this.surrender = function()  {
+		solve();
+		// pause
+		// user selects input
+	}
+}
