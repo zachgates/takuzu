a42a38040828743cc24236d9ea18ef1bf6074765
Zach <zachgatestech@gmail.com>
2015-03-16 20:35:12 -0500

    game objects

---
+++ js/block.js
@@ -0,0 +1,28 @@
+function Block(x, y, elemid, parent) {
+	GameObject.call(this);
+	var self = this;
+	
+	self.x = x;
+	self.y = y;
+	self.parent = parent;
+	
+	generate();
+	
+	function generate() {
+		self.element = document.createElement('td');
+		self.element.className = 'noselect Cell CellState1';
+		self.element.id = elemid;
+		self.element.onclick = function(){
+			if (self.parent.getState() > 0) {
+				nextState = self.getState() + 1;
+				if (nextState > 3) {
+					nextState = 1;
+				}
+				self.setState(nextState);
+				this.className = 'noselect Cell CellState' + nextState.toString();
+			}
+		};
+	}
+}
+
+Block.prototype = Object.create(GameObject.prototype);
---
+++ js/table.js
@@ -0,0 +1,55 @@
+var Levels = [3, 4, 5, 6];
+
+function GameGrid(scale) {
+	GameObject.call(this);
+	var self = this;
+	
+	self.s = scale;
+	if (Levels.indexOf(self.s) == -1) {
+		self.s = 3;
+	}
+	
+	self.cells = new Object();
+	
+	generate();
+	
+	function generate() {
+		self.element = document.createElement('table');
+		self.element.className = 'Table noselect';
+		self.element.id = 'GameTable';
+		self.element.remove = function () {
+			this.parentElement.removeChild(this);
+		}
+		self.elementBody = document.createElement('tbody');
+		self.element.appendChild(self.elementBody);
+		
+		for (var row = 0; row < self.s; row++) {
+			rowElement = document.createElement('tr');
+			rowElement.className = 'Row noselect';
+			rowElement.id = 'GameRow' + (row+1).toString();
+			rowElement.height = (self.element.height) / self.s;
+			for (var cell = 0; cell < self.s; cell++) {
+				elemid = 'GameCell' + (row+1).toString() + 'x' + (cell+1).toString();
+				cellElement = new Block(row, cell, elemid, self);
+				cellElement.element.height = (self.element.height) / self.s;
+				self.cells[elemid] = cellElement
+				rowElement.appendChild(cellElement.element);
+			}
+			self.elementBody.appendChild(rowElement);
+		}
+		
+		document.body.appendChild(self.element);
+	}
+	
+	var parentDestroy = this.destroy;
+	this.destroy = function () {
+		parentDestroy();
+		self.cells = null;
+		self.s = null;
+		self.element.remove();
+		self.element = null;
+		self = null;
+	}
+}
+
+GameGrid.prototype = Object.create(GameObject.prototype);
