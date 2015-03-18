d5593b875cf2300b293620dcad5dad351ebfd7b7
michaelpri <michaelpri10@gmail.com>
2015-03-18 11:48:10 -0400

    Make restart button work

--- js/game.js
+++ js/game.js
@@ -1,35 +1,44 @@
 function Game(scale) {
 	var self = this;
-	
+
 	generate();
-	
+
 	function generate() {
 		self.puzzle = new GameGrid(scale, this);
 	}
-	
+
 	function solve() {
 		// fade in solution
 	}
-	
+
 	function exit() {
 		// fade back to menu
 	}
-	
+
 	this.endGame = function() {
 		// fade out
 		// exit
 	}
-	
+
 	this.surrender = function()  {
 		solve();
 		// pause
 		// user selects input
 	}
-	
+
 	this.update = function(puzzle) {
 		self.puzzle = puzzle;
 		for (var cell in self.puzzle.cells) {
 			// check for discrepancies
 		}
 	}
+
+	gameRestart = function() {
+		$("#restart").click(function() {
+			$("#GameTable").remove();
+			generate();
+		})
+	}
+	
+	$(document).ready(gameRestart)
 }
